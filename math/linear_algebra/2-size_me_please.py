a056ae30cd9872efeb9ccec27ae9f2565cd30bd4
Luiyi-Hurtado <luiyifernando@gmail.com>
2023-03-21 18:06:49 +0000

    [task-2]: add PEP8 again

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -2,10 +2,15 @@
 def matrix_shape(matrix):
     """Return shape from a matrix
 
-    args(matrix):
-        list of lists with contend integers
-    return(shape):
-        list of integer with the size of the matrix
+    Parameters
+    -----------
+    matrix : list of list
+        with integers
+
+    Return
+    -------
+    list
+        a list of integer with the size of the matrix
     """
     shape = []
     while isinstance(matrix, list):
