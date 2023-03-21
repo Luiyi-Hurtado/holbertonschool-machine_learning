a5c4887ce662f35e955d8cf603854ba6de885d74
Luiyi-Hurtado <luiyifernando@gmail.com>
2023-03-21 17:59:49 +0000

    [task-2]: add docstring in my function

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
+    """
+    Return shape from a matrix
+
+    args(matrix):
+        list of lists with contend integers
+    return(shape): 
+        list of integer with the size of the matrix
+    """
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
