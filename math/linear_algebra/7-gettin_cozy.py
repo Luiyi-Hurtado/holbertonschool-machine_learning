0326baff8da7180c56bb82812a20bf99a54e1322
Luiyi-Hurtado <luiyifernando@gmail.com>
2023-03-22 18:56:26 +0000

    [TASK-7]: script for concatenate a two matrices with specific axis

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -2,7 +2,29 @@
 """Function for concatenate two matrices"""
 
 
-def cat_matrices2D(mat1, mat2):
+def cat_matrices2D(mat1, mat2, axis=0):
+    """ 
+    Return concatenates two matrices along specific axis
+
+    Args:
+        mat1 (list of list):
+            the first matrix for concatenate
+        mat2 (list of list):
+            the second matrix for concatenate right to the mat1
+        axis (int):
+            specifies the axis along which to concatenate the matrices (default is 0)
+
+    Returns:
+        list of lists: concatenate matrix with different axis
     """
-    """
-    pass
+    cat_mat = []
+    if axis == 0:
+        if len(mat1[0]) == len(mat2[0]):
+            cat_mat = mat1 + mat2
+            return cat_mat
+    elif axis == 1:
+        if len(mat1) == len(mat2):
+            for indx in range(len(mat1)):
+                cat_mat.append(mat1[indx] + mat2[indx])
+            return cat_mat
+    return None
