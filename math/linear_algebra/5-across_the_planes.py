9634ca4662f23125eaf313f329a330d87aed0d45
Luiyi-Hurtado <luiyifernando@gmail.com>
2023-03-22 02:08:58 +0000

    [TASK-5]: script that return a matrix with the result of the two matrices

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Function for create a matrix with the add of two matrices"""
+
+
+def add_matrices2D(mat1, mat2):
+    """
+    Return list the add matrix if the matrices are
+    the same shape
+
+    Parameters
+    ----------
+    mat1 : list
+        list of the lists with integers
+    mat2 : list
+        list of the lists with integers
+
+    Return
+    ------
+    List : new array
+        a list content the add result from the two matrices
+    None : Boolean
+        if the shape of matrices are not the same
+    """
+    len_mat1 = len(mat1)
+    len_mat2 = len(mat2[0])
+    add_mat = [[0, 0], [0, 0]]
+
+    if not (len_mat1 == len_mat2):
+        return None
+    for row in range(len_mat1):
+        for colum in range(len_mat2):
+            add_mat[row][colum] = mat1[row][colum] + mat2[row][colum]
+    return add_mat
