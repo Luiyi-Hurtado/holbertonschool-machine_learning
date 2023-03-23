2239a8ef8a4d2ec9b5f469f9303be83146b654cb
Luiyi-Hurtado <luiyifernando@gmail.com>
2023-03-23 16:18:34 +0000

    [TASk-10]: create script that calculates the shape from a np.array

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Fucntion that calculaete the shape of an array"""
+
+
+def np_shape(matrix):
+    """
+    Return a shape from a numpy.ndarray
+
+    Args:
+        matrix (list of list):
+            a matrix with different shape
+
+    Return:
+        tuple : 
+            a tuple with the size of the matrix
+    """
+    return matrix.shape
