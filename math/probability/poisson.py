2a7b113cb4425e9b122cce295aa998e983dc5acc
Luiyi-Hurtado <luiyifernando@gmail.com>
2023-04-04 17:01:33 +0000

    [TASK-0]: create a README file and create the class poisson for this calculate poisson distribution

---
+++ math/probability/poisson.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+This module contains the Poisson class,
+which represents a Poisson distribution.
+"""
+
+
+class Poisson:
+    """
+    Class representing a Poisson distribution.
+
+    Parameters:
+        data (list)
+            A list the date to be used to estimate the distribution.
+        lambths (float)
+            The expected number of occurrences in a given time frame.
+
+    Attributes:
+        lambtha (float)
+            The expected number of occurrences in a given time frame.
+
+    Raises:
+        ValueError:
+            If lambtha is not a positive value or equals to 0.
+            If data is given but it´s not a list.
+            If data is given but doesn´t at least two data points.
+    """
+
+    def __init__(self, data=None, lambtha=1.):
+        """
+        Initialize a Poisson distrubucion with the fiven paramters.
+
+        If data is not given, set lambtha to the given value.
+        If data is given, calculate the lambtha of data
+        """
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            if lambtha > 0:
+                self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise ValueError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = sum(data) / len(data)
