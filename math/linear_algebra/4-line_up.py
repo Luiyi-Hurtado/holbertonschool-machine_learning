11506026210d7533e8a2adc41b97a357059bd9d5
Luiyi-Hurtado <luiyifernando5@gmail.com>
2023-07-29 13:46:20 -0500

    TASK 3: create module that provides a functions for adds two array element-wise

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+This module provides function for that adds two arrays
+
+The function in this module take two arrays as input and
+returns a new array with the add numbers
+"""
+
+
+def add_arrays(arr1, arr2):
+    """
+    Adds two arrays element-wise
+
+    Args:
+        arr1 (list): array of numbers 
+        arr2 (list): array of numbers
+    Returns:
+        add_list: new list with add numbers
+    """
+    pass
+    if len(arr1) != len(arr2):
+        return None
+    add_list = [a + b for a, b in zip(arr1, arr2)]
+    return add_list
