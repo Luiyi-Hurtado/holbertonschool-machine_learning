2833b606b48580dc371f87bac058da351b56ccda
Luiyi-Hurtado <luiyifernando5@gmail.com>
2023-07-29 22:03:35 -0500

    TASK 10: create module that calculate a shape from a matrix

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module provides function that calculate the shape of a np.matrix.
+
+This module take a matrix with different Dimension an calculate the shape.
+"""
+
+
+def np_shape(matrix):
+    """
+    Calculate the shape of a matrix.
+
+    Args:
+        matrix (list): matrix to calculate the shape.
+    Return:
+        Shape of the matrix
+    """
+    return matrix.shape
