e5e0f4581a6a4c0186b6809f8bc350ebd4f8a3f9
Luiyi-Hurtado <luiyifernando5@gmail.com>
2023-07-29 22:43:31 -0500

    TASK 13: create the module for concatenate two matrices and add more documentation

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """
+This module provide function tha concatenate two matrices.
 """
 import numpy as np
 
@@ -9,9 +10,9 @@ def np_cat(mat1, mat2, axis=0):
     Concatenate two matrices
 
    Args:
-        mat1 (list): The fitst matrix to concatenate
-        mat2 (list): The second matrix to concatenate
-        axis (list): The axis along which to concatenate the matrices.
+        mat1 (list): The fitst matrix to concatenate.
+        mat2 (list): The second matrix to concatenate.
+        axis (list): The axis along which to concatenate the matrices
             0 for rows, 1 for columns.
     Return:
         The concatenated matrix.
